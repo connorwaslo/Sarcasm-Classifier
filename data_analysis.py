30aae7b2a89660fa9b4bdc90ce13201eb08c724b
connorwaslo <connorwaslo@gmail.com>
2019-05-17 23:30:07 -0700

    Visualize word frequency
    Tokenize headlines and eliminate stopwords. Visualize counts into a bar graph.

--- data_analysis.py
+++ data_analysis.py
@@ -1,22 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
 import nltk
+import string
 
 pd.set_option('display.max_columns', 5)
 
 data = pd.read_json('Sarcasm_Headlines_Dataset.json', lines=True)
-print(data)
-
 category_counts = data['is_sarcastic'].value_counts()
 
-# Data split
-plt.pie(category_counts, labels=['Not Sarcastic', 'Sarcastic'])
-plt.show()
+# Visualize data split
+# plt.pie(category_counts, labels=['Not Sarcastic', 'Sarcastic'])
+# plt.show()
 
-headlines = data['headline'].str.lower().str.replace(r'\|', ' ').str.cat(sep=' ')
-words = nltk.tokenize.word_tokenize(headlines)
-word_dist = nltk.FreqDist(words)
+tokenizer = TweetTokenizer()
+headlines = data.apply(lambda row: tokenizer.tokenize(row['headline']), axis=1)
+# print(headlines)
+
+all_words = []
+stops = set(stopwords.words('english'))
+for headline in headlines:
+    for word in headline:
+        if word not in stops and word not in string.punctuation:
+            all_words.append(word)
+
+word_dist = nltk.FreqDist(all_words)
 
 result = pd.DataFrame(word_dist.most_common(25), columns=['Word', 'Frequency']).set_index('Word')
 print(result)
